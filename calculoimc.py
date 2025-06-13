587539c6cedf92558d6bec3ad7b9362e73ead392
Aisla <severoaisla@gmail.com>
2025-06-13 15:20:14 -0300

    Create calculoimc.py

---
+++ calculoimc.py
@@ -0,0 +1,8 @@
+
+#calcula o imc do merda
+
+altura = float  (input("Digite a merda da tua altura : "))
+peso = float(input("me fale o seu pesinho amigo: "))
+def calculo(IMC):
+    print(f"o seu imc é:{IMC}" )
+calculo(peso / altura **2)
